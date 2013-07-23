3bda4a73f9d6961ce2f73273359667b67b24718d
siosbigdata <siosbigdata@gmail.com>
2013-07-23 10:04:51 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shirokane::Application.config.secret_key_base = '5635eb9cdb4b031e3d0335c2c77750860704807bb17f7180b486839b1db58dbde17dd541431730e87cf638a6a6329082300ed9d20a97d23496284a17c5078ca8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shirokane::Application.config.session_store :cookie_store, key: '_shirokane_session'
