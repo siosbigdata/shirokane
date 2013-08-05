3fde17f4f98c7c938c3c68525c3b5d17e59248a8
siosbigdata <siosbigdata@gmail.com>
2013-08-05 15:19:47 +0900

    commit31

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   protect_from_forgery
-    
+  
+  # 通常ユーザ用-現在のアカウント設定
+  def application_title
+    settings = Setting.where(:name => 'title')
+    @application_title = settings[0].parameter
+  end
+  helper_method :application_title
 end
