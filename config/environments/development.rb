1a521eabb1de149175762a3daef0972a9fa3419a
siosbigdata <siosbigdata@gmail.com>
2013-08-19 16:55:25 +0900

    chagne2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,7 @@ Shirokane::Application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.logger = Logger.new("log/development.log", 'daily')
+  Rails.logger.level = Logger::debug
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
