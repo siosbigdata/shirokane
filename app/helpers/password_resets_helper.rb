93fa0fb27a2cd47f3167bd0837ea4bfe2947d552
siosbigdata <siosbigdata@gmail.com>
2013-09-02 13:41:36 +0900

    add comments

--- app/helpers/password_resets_helper.rb
+++ app/helpers/password_resets_helper.rb
@@ -1,2 +1,7 @@
+#coding: utf-8
+# PasswordResetsHelper
+# Author:: Kazuko Ohmura
+# Date:: 2013.08.30
+
 module PasswordResetsHelper
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
+#coding: utf-8
+# UserMailer
+# Author:: Kazuko Ohmura
+# Date:: 2013.08.30
+
+# メール送信用
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
