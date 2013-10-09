38c88e57a31215e90eea21501c5b0c37e25af03b
siosbigdata <siosbigdata@gmail.com>
2013-10-09 13:10:05 +0900

    add seeds change readme.md

--- README.md
+++ README.md
@@ -2,21 +2,50 @@ OSS Dashboard
 =========
 This system is a dashboard of BigData graph.  
 
+Install
+=========
+* Prepare the environment to the database which can be connected to the Rails.
+* Please perform the following settings in your environment
+    $ git clone https://github.com/siosbigdata/shirokane.git newsite
+    $ vi config/database.yml (Please prepare by yourself is not included in the project.)
+    $ vi config/environments/production.rb (Fix the host of the last line)
+    $ cd newsite
+    $ mkdir log
+    $ chmod 777 log
+    $ bundle install
+    $ rake db:setup
+
+Create Graph
+=========
+* create table
+    If you want to make "test01" table in database (ex. postgresql)
+    > create table td_test01 (td_time timestamp with time zone,td_count decimal);
+    
+    $ rails c
+    > Graph.create(:name=>"test01",:title=>"テスト０１",:analysis_type=>0,:graph_type=>0,:term=>2,:y=>"count",:y_min=>0,:y_max_time=>100,:y_max_day=>1000,:y_max_month=>1000,:useval=>0,:useshadow=>0,:usetip => 0,:linewidth=>2,:template=>"white-dimgray",:usepredata=>0,:uselastyeardata=>0)
+    
+Use Treasure Data Push
+=========
+* One time.
+    td query --result 'postgresql://newuser:newusernewuser@175.41.253.164/newdb/td_access'  -w -d sios_web    "SELECT TD_TIME_FORMAT(time, 'yyyy-MM-dd HH:00:00 +09:00') as td_time , COUNT(1) as td_count FROM www_access GROUP BY TD_TIME_FORMAT(time, 'yyyy-MM-dd HH:00:00 +09:00') " 
+
+* If you run this push on a regular basis,you need to make "td shced"
+
 Attention
 =========
-This system takes advantage of PostgreSQL.  
-This system is made ​​by the basis of Ruby on Rails4.  
-This system is compatible with Japanese and English. Please create a language file when using other languages​​.  
-Please be careful config/database.yml file because not enough.
+* This system takes advantage of PostgreSQL.  
+* This system is made ​​by the basis of Ruby on Rails4.  
+* This system is compatible with Japanese and English. Please create a language file when using other languages​​.  
+* Please be careful config/database.yml file because not enough.
 
 License
 =========
-Copyright &copy; 2013 SIOS Technology,Inc.  
-Licensed under the [Apache License, Version 2.0][Apache]  
-Distributed under the [MIT License][mit].  
-Dual licensed under the [MIT license][MIT] and [GPL license][GPL].  
+* Copyright &copy; 2013 SIOS Technology,Inc.  
+* Licensed under the [Apache License, Version 2.0][Apache]  
+* Distributed under the [MIT License][mit].  
+* Dual licensed under the [MIT license][MIT] and [GPL license][GPL].  
 
-Part of chart  
+* Part of chart  
 [Copyright (c) 2013 Toshiro Takahashi][CCCHART]  
   
 [Apache]: http://www.apache.org/licenses/LICENSE-2.0  
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#coding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(
+  :name => "admin", 
+  :password => "admin",
+  :password_confirmation => "admin",
+  :title=>'管理者',
+  :mail=>'admin@admin.com',
+  :group_id=> 1,:admin=>true)
+
+Group.create(:name => "admingroup", :title => "管理グループ")
+
+Setting.create(:name  => "admin_mail",  :title  =>  "管理者：メール", :parameter => "admin@sios.com",:vieworder => 1,:columntype => 0)
+Setting.create(:name => "title" ,:title => "アプリケーションタイトル",:parameter => "OSS-DashBoard" ,:vieworder => 2,:columntype => 0)
+Setting.create(:name => "dashboardnum" ,:title => "ダッシュボードに並べるグラフ数",:parameter => "3" ,:vieworder => 10,:columntype => 3)
+Setting.create(:name => "sessionnum" ,:title => "同時ログイン数",:parameter => "3" ,:vieworder => 0,:columntype => 3)
+Setting.create(:name => "servicename" ,:title => "サービス名",:parameter => "sitename" ,:vieworder => 0,:columntype => 0) # 一意なservicenameを指定
+Setting.create(:name => "csvdownloadsize" ,:title => "最大CSVダウンロード容量",:parameter => "32212254720" ,:vieworder => 0,:columntype => 3) # 上限30GB
+Setting.create(:name => "maxuser" ,:title => "最大ユーザ数",:parameter => "5" ,:vieworder => 0,:columntype => 3) #上限5
+Setting.create(:name => "useMarker" ,:title => "マーカーの種類",:parameter => "css-ring" ,:vieworder => 0,:columntype => 0) # css-maruも指定可能
+Setting.create(:name => "markerWidth" ,:title => "マーカーの大きさ",:parameter => "1" ,:vieworder => 11,:columntype => 3)
+
+Graphtemplate.create(:name=>'aliceblue-black',:linecolor => "#000000",:bgfrom => "#f0f8ff",:bgto => "#f0f8ff",:textcolor => "#000000",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b" )
+Graphtemplate.create(:name=>'aliceblue-dimgray',:linecolor => "#696969",:bgfrom => "#f0f8ff",:bgto => "#f0f8ff",:textcolor => "#696969",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b" )
+Graphtemplate.create(:name=>'black-red',:linecolor => "#ff0000",:bgfrom => "#687478",:bgto => "#222222",:textcolor => "#ff0000",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'black-white',:linecolor => "#ffffff",:bgfrom => "#687478",:bgto => "#222222",:textcolor => "#ffffff",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'cornsilk-dimgray',:linecolor => "#696969",:bgfrom => "#fff8dc",:bgto => "#fff8dc",:textcolor => "#696969",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'green-green',:linecolor => "#003300",:bgfrom => "#00ff99",:bgto => "#339933",:textcolor => "#003300",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'lavender-dimgray',:linecolor => "#696969",:bgfrom => "#e6e6fa",:bgto => "#e6e6fa",:textcolor => "#696969",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'lavenderblush-dimgray',:linecolor => "#696969",:bgfrom => "#fff0f5",:bgto => "#fff0f5",:textcolor => "#696969" ,:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'lightcyan-dimgray',:linecolor => "#696969",:bgfrom => "#e0ffff",:bgto => "#e0ffff",:textcolor => "#696969",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b" )
+Graphtemplate.create(:name=>'lightsteelblue-dimgray',:linecolor => "#696969",:bgfrom => "#e0ffff",:bgto => "#b0c4de",:textcolor => "#696969" ,:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'mistyrose-dimgray',:linecolor => "#696969",:bgfrom => "#ffe4e1",:bgto => "#ffe4e1",:textcolor => "#696969" ,:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'snow-dimgray',:linecolor => "#696969",:bgfrom => "#fffafa",:bgto => "#fffafa",:textcolor => "#696969",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'white-black',:linecolor => "#000000",:bgfrom => "#ffffff",:bgto => "#ffffff",:textcolor => "#000000",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'white-dimgray',:linecolor => "#696969",:bgfrom => "#ffffff",:bgto => "#ffffff",:textcolor => "#696969" ,:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+Graphtemplate.create(:name=>'yellow-orange',:linecolor => "#ff6600",:bgfrom => "#ffff66",:bgto => "#ffcc33",:textcolor => "#ff6600",:linecolor_pre => "#90ee90",:linecolor_last => "#bdb76b")
+
+
